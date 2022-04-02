ba73cc7ab6fde23ebd677b19347b5a867157b472
Leonardo Bratfisch <sevalhosleonardo@gmail.com>
2022-04-02 15:34:28 -0300

    Add project files.

---
+++ ExemploWebAPI/Controllers/ClientesController.cs
@@ -0,0 +1,26 @@
+﻿using ExemploWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploWebAPI.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        public List<Cliente> clientes = new List<Cliente>();
+
+        public List<Cliente> Get() 
+        {
+            return clientes;
+        }
+
+        public void Post(string nome)
+        {
+            if(!string.IsNullOrEmpty(nome))
+                clientes.Add(new Cliente(nome));
+        }
+    }
+}
